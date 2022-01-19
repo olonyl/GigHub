8bc8ad0f02d93bba463eafd6f143d0d6cffc7c4a
olonyl <olony.rocha@contollo.net>
2022-01-19 09:18:37 -0600

    Implemented Anti Forgery Token for CSRF

--- GigHub.WebApplication/Controllers/GigsController.cs
+++ GigHub.WebApplication/Controllers/GigsController.cs
@@ -28,6 +28,7 @@ namespace GigHub.WebApplication.Controllers
 
         [Authorize]
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Create(GigFormViewModel viewModel)
         {
             if (!ModelState.IsValid)
