627f1bc0c194ee0a2e2489dc8d3eca6cd8040c0e
olonyl <olony.rocha@contollo.net>
2022-01-21 21:40:38 -0600

    Improve Look and Feel for the Gig's List

--- GigHub.WebApplication/Controllers/HomeController.cs
+++ GigHub.WebApplication/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ namespace GigHub.WebApplication.Controllers
         {
             var upcomingGigs = _context.Gigs
                 .Include(g => g.Artist)
+                .Include(g => g.Genre)
                 .Where(g => g.DateTime > DateTime.Now);
 
             return View(upcomingGigs);
